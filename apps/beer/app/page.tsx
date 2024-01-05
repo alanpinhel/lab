c46c271848f8f15777619f3ab0b593fedea8e3bc
Alan Pinhel <alanpinhel@gmail.com>
2024-01-05 18:57:50 -0300

    chore: adiciona cor primaria

--- apps/beer/app/page.tsx
+++ apps/beer/app/page.tsx
@@ -1,10 +1,3 @@
-import { Text, Title } from "@mantine/core";
-
 export default function Home() {
-  return (
-    <>
-      <Text>Hello World</Text>
-      <Title order={1}>Hello World</Title>
-    </>
-  );
+  return <></>;
 }
--- apps/beer/theme.ts
+++ apps/beer/theme.ts
@@ -1,8 +1,13 @@
 "use client";
 
+import { generateColors } from "@mantine/colors-generator";
 import { createTheme } from "@mantine/core";
 import { poppins } from "./fonts";
 
 export const theme: any = createTheme({
   fontFamily: poppins.style.fontFamily,
+  primaryColor: "yellow",
+  colors: {
+    yellow: generateColors("#F4C314"),
+  },
 });
