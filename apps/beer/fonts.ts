bec33476bfb15c8dfc835470160326dd62eac648
Alan Pinhel <alanpinhel@gmail.com>
2024-01-15 11:08:59 -0300

    feat: fonts

--- apps/beer/fonts.ts
+++ apps/beer/fonts.ts
@@ -1,6 +1,6 @@
 import { Allerta_Stencil, Poppins, Train_One } from "next/font/google";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 const trainOne = Train_One({ subsets: ["latin"], weight: ["400"] });
 const allertaStencil = Allerta_Stencil({ subsets: ["latin"], weight: ["400"] });
 
