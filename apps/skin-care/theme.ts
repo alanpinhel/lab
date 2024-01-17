0d292bfc23053e93bc54d5b20aea3061cc9e76cf
Alan Pinhel <alanpinhel@gmail.com>
2024-01-17 19:36:20 -0300

    feat: skin care app assets

--- apps/skin-care/theme.ts
+++ apps/skin-care/theme.ts
@@ -1,5 +1,12 @@
 "use client";
 
+import { generateColors } from "@mantine/colors-generator";
 import { createTheme } from "@mantine/core";
+import { apercu } from "./fonts";
 
-export const theme: any = createTheme({});
+export const theme: any = createTheme({
+  fontFamily: apercu.style.fontFamily,
+  colors: {
+    blue: generateColors("#020953"),
+  },
+});
