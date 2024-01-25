7454b2e68d1ae70db70979269f4d5c32dbb1f5ca
Alan Pinhel <alanpinhel@gmail.com>
2024-01-25 18:21:54 -0300

    feat: create next app

---
+++ apps/watch-store/app/layout.tsx
@@ -0,0 +1,27 @@
+import "@mantine/core/styles.css";
+
+import { theme } from "@/theme";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Watch Store",
+  description: `watch store app interface to train front-end skills`,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
