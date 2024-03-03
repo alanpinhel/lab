27fe291d3f8aa063f1b3fa2c34f4a1d75e2e12c4
Alan Pinhel <alanpinhel@gmail.com>
2024-03-03 13:39:17 -0300

    feat: header

--- apps/ui-challenges/app/dokterian/page.tsx
+++ apps/ui-challenges/app/dokterian/page.tsx
@@ -1,4 +1,18 @@
-import { Container } from "@mantine/core";
+import {
+  ActionIcon,
+  Avatar,
+  Card,
+  Container,
+  Divider,
+  Group,
+  Stack,
+  Text,
+} from "@mantine/core";
+import {
+  IconCalendar,
+  IconChevronRight,
+  IconClock,
+} from "@tabler/icons-react";
 
 export default function DokterianHomePage() {
   return (
@@ -8,6 +22,51 @@ export default function DokterianHomePage() {
       px={24}
       py={20}
       size="xs"
-    ></Container>
+    >
+      <Group mb="xl" justify="space-between">
+        <Stack gap={0}>
+          <Text c="dimmed">Hello,</Text>
+          <Text fw={700} fz="lg">
+            Hi James
+          </Text>
+        </Stack>
+
+        <Avatar size="lg" src="https://i.pravatar.cc/300" />
+      </Group>
+
+      <Card bg="primary" radius="lg" padding="lg">
+        <Group justify="space-between">
+          <Group>
+            <Avatar
+              size="lg"
+              src="https://i.pravatar.cc/300"
+            />
+            <Stack gap={0}>
+              <Text c="white" fw={700} fz="lg">
+                Dr. Imran Syahir
+              </Text>
+              <Text c="gray.2">General Doctor</Text>
+            </Stack>
+          </Group>
+
+          <ActionIcon>
+            <IconChevronRight stroke={1.5} />
+          </ActionIcon>
+        </Group>
+
+        <Divider my="lg" color="gray.1" />
+
+        <Group gap="lg">
+          <Group gap={4} c="white">
+            <IconCalendar stroke={1.5} />
+            <Text fz="sm">Sunday, 12 June</Text>
+          </Group>
+          <Group gap={4} c="white">
+            <IconClock stroke={1.5} />
+            <Text fz="sm">11:00 - 12:00 AM</Text>
+          </Group>
+        </Group>
+      </Card>
+    </Container>
   );
 }
--- apps/ui-challenges/app/dokterian/theme.ts
+++ apps/ui-challenges/app/dokterian/theme.ts
@@ -1,14 +1,10 @@
 "use client";
 
-import { generateColors } from "@mantine/colors-generator";
 import { createTheme } from "@mantine/core";
 import { poppins } from "./fonts";
 
 export const theme: any = createTheme({
   fontFamily: poppins.style.fontFamily,
-  colors: {
-    blue: generateColors("#4894fe"),
-  },
   headings: {
     fontFamily: poppins.style.fontFamily,
   },
